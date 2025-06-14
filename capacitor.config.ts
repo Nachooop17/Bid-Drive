53c183c6a3a4ca26783b9bc586c409b778dc3b48
Nachooop17 <mic.ibanez@duocuc.cl>
2025-06-13 22:45:40 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Bid Drive',
+  webDir: 'www'
+};
+
+export default config;
